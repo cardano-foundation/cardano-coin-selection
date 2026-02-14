af568359c477716ed80f1be76275969c8d4bdcc0
Paolo Veronelli <paolo.veronelli@gmail.com>
2026-02-14 10:34:04 +0000

    chore: add flake.nix and cabal.project for Nix builds
    - flake.nix with haskell.nix, GHC 9.12.2
    - nix/project.nix with dev shell (cabal, HLS, fourmolu, hlint, hoogle)
    - cabal.project with Hackage index-state
    - Update CI to use flake outputs

---
+++ nix/project.nix
@@ -0,0 +1,37 @@
+{ indexState, pkgs, ... }:
+
+let
+  shell = { pkgs, ... }: {
+    tools = {
+      cabal = { index-state = indexState; };
+      cabal-fmt = { index-state = indexState; };
+      haskell-language-server = { index-state = indexState; };
+      hoogle = { index-state = indexState; };
+      fourmolu = { index-state = indexState; };
+      hlint = { index-state = indexState; };
+    };
+    withHoogle = true;
+    buildInputs = [
+      pkgs.just
+      pkgs.nixfmt-classic
+      pkgs.shellcheck
+    ];
+  };
+
+  mkProject = { lib, pkgs, ... }: {
+    name = "cardano-coin-selection";
+    src = ./..;
+    compiler-nix-name = "ghc9122";
+    shell = shell { inherit pkgs; };
+  };
+
+  project = pkgs.haskell-nix.cabalProject' mkProject;
+
+in {
+  devShells.default = project.shell;
+  inherit project;
+  packages.lib =
+    project.hsPkgs.cardano-coin-selection.components.library;
+  packages.unit-tests =
+    project.hsPkgs.cardano-coin-selection.components.tests.unit;
+}
