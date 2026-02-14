dff902bc7100fc6916560ec2068c8d94f5a98e21
Paolo Veronelli <paolo.veronelli@gmail.com>
2026-02-14 13:16:21 +0000

    chore: add mkdocs to nix dev shell

--- flake.nix
+++ flake.nix
@@ -2,18 +2,29 @@
   description = "Coin selection algorithms for the Cardano blockchain";
   nixConfig = {
     extra-substituters = [ "https://cache.iog.io" ];
-    extra-trusted-public-keys =
-      [ "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ=" ];
+    extra-trusted-public-keys = [ "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ=" ];
   };
   inputs = {
     haskellNix.url = "github:input-output-hk/haskell.nix";
     nixpkgs.follows = "haskellNix/nixpkgs-unstable";
     flake-utils.url = "github:hamishmack/flake-utils/hkm/nested-hydraJobs";
+    mkdocs.url = "github:paolino/dev-assets?dir=mkdocs";
   };
 
-  outputs = inputs@{ self, nixpkgs, flake-utils, haskellNix, ... }:
-    let version = self.dirtyShortRev or self.shortRev;
-    in flake-utils.lib.eachSystem [ "x86_64-linux" "aarch64-darwin" ] (system:
+  outputs =
+    inputs@{
+      self,
+      nixpkgs,
+      flake-utils,
+      haskellNix,
+      mkdocs,
+      ...
+    }:
+    let
+      version = self.dirtyShortRev or self.shortRev;
+    in
+    flake-utils.lib.eachSystem [ "x86_64-linux" "aarch64-darwin" ] (
+      system:
       let
         pkgs = import nixpkgs {
           overlays = [ haskellNix.overlay ];
@@ -22,9 +33,14 @@
         project = import ./nix/project.nix {
           indexState = "2025-10-01T00:00:00Z";
           inherit pkgs;
+          mkdocs = mkdocs.packages.${system};
         };
-      in {
-        packages = project.packages // { default = project.packages.lib; };
+      in
+      {
+        packages = project.packages // {
+          default = project.packages.lib;
+        };
         inherit (project) devShells;
-      });
+      }
+    );
 }
--- nix/project.nix
+++ nix/project.nix
@@ -1,31 +1,56 @@
-{ indexState, pkgs, ... }:
+{
+  indexState,
+  pkgs,
+  mkdocs,
+  ...
+}:
 
 let
-  shell = { pkgs, ... }: {
-    tools = {
-      cabal = { index-state = indexState; };
-      haskell-language-server = { index-state = indexState; };
-      hoogle = { index-state = indexState; };
-      fourmolu = { index-state = indexState; };
-      hlint = { index-state = indexState; };
+  shell =
+    { pkgs, ... }:
+    {
+      tools = {
+        cabal = {
+          index-state = indexState;
+        };
+        haskell-language-server = {
+          index-state = indexState;
+        };
+        hoogle = {
+          index-state = indexState;
+        };
+        fourmolu = {
+          index-state = indexState;
+        };
+        hlint = {
+          index-state = indexState;
+        };
+      };
+      withHoogle = true;
+      buildInputs = [
+        pkgs.just
+        pkgs.nixfmt-classic
+        pkgs.shellcheck
+        pkgs.mkdocs
+        mkdocs.from-nixpkgs
+      ];
     };
-    withHoogle = true;
-    buildInputs = [ pkgs.just pkgs.nixfmt-classic pkgs.shellcheck ];
-  };
 
-  mkProject = { lib, pkgs, ... }: {
-    name = "cardano-coin-selection";
-    src = ./..;
-    compiler-nix-name = "ghc9122";
-    shell = shell { inherit pkgs; };
-  };
+  mkProject =
+    { lib, pkgs, ... }:
+    {
+      name = "cardano-coin-selection";
+      src = ./..;
+      compiler-nix-name = "ghc9122";
+      shell = shell { inherit pkgs; };
+    };
 
   project = pkgs.haskell-nix.cabalProject' mkProject;
 
-in {
+in
+{
   devShells.default = project.shell;
   inherit project;
   packages.lib = project.hsPkgs.cardano-coin-selection.components.library;
-  packages.unit-tests =
-    project.hsPkgs.cardano-coin-selection.components.tests.unit;
+  packages.unit-tests = project.hsPkgs.cardano-coin-selection.components.tests.unit;
 }
